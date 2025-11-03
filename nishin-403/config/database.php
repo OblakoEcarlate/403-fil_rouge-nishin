729555b029bdec99ef417787264c8665a6aaa8df
Nina <nina.gmz115@gmail.com>
2025-11-03 13:54:30 +0100

    changement de port

--- nishin-403/config/database.php
+++ nishin-403/config/database.php
@@ -67,7 +67,7 @@ return [
         'mongodb' => [
             'driver' => 'mongodb',
             'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', 27022),
+            'port' => env('DB_PORT', 27017),
             'database' => env('DB_DATABASE', 'nishin-403'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
