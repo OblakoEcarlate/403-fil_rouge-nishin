6fa8339b244731228fd344aaf6f8145125c435bd
Nina <nina.gmz115@gmail.com>
2025-11-03 14:23:03 +0100

    chore: align mongo defaults across envs

--- nishin-403/config/database.php
+++ nishin-403/config/database.php
@@ -68,7 +68,7 @@ return [
             'driver' => 'mongodb',
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'nishin-403'),
+            'database' => env('DB_DATABASE', 'nishin_403'),
             'username' => env('DB_USERNAME', 'root'),
             'password' => env('DB_PASSWORD', 'root'),
             'options' => [
